b4cfd22522ccb4faef1b2617edef274f9cb6118c
Mark Bannister <mspbannister@gmail.com>
2017-04-25 10:07:30 +0100

    Update 'validation'

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -12,6 +12,7 @@
 
 import pickle
 import sys
+from time import time
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
@@ -28,5 +29,24 @@ labels, features = targetFeatureSplit(data)
 
 
 ### it's all yours from here forward!  
-
-
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.cross_validation import train_test_split
+features_train, features_test, labels_train, labels_test = train_test_split(
+    features, labels, test_size=0.3, random_state=42)
+# min_samples_split=40
+clf = DecisionTreeClassifier()#min_samples_split=min_samples_split)
+print "Fitting model..."
+t0 = time()
+clf = clf.fit(features_train, labels_train)
+print "Creating predictions..."
+t1 = time()
+pred = clf.predict(features_test)
+t2 = time()
+from sklearn.metrics import accuracy_score
+print "Computing accuracy..."
+acc = accuracy_score(pred, labels_test)
+timeDiff = round(t1-t0, 3)
+print "Training time: "+str(timeDiff)+"s"
+timeDiff = round(t2-t1, 3)
+print "Prediction time: "+str(timeDiff)+"s"
+print "Accuracy: "+str(acc)
