f9f2b71d778f30c0d509db13dddd45d24b19fd30
Mark Bannister <mspbannister@gmail.com>
2017-04-25 09:47:57 +0100

    Update 'evaluation'

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -13,6 +13,7 @@
 
 import pickle
 import sys
+from time import time
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
@@ -27,5 +28,28 @@ labels, features = targetFeatureSplit(data)
 
 
 ### your code goes here 
-
-
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.cross_validation import train_test_split
+features_train, features_test, labels_train, labels_test = train_test_split(
+    features, labels, test_size=0.3, random_state=42)
+# min_samples_split=40
+clf = DecisionTreeClassifier()#min_samples_split=min_samples_split)
+print "Fitting model..."
+t0 = time()
+clf = clf.fit(features_train, labels_train)
+print "Creating predictions..."
+t1 = time()
+pred = clf.predict(features_test)
+t2 = time()
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+print "Computing accuracy..."
+acc = accuracy_score(labels_test, pred)
+prec = precision_score(labels_test, pred)
+rec = recall_score(labels_test, pred)
+timeDiff = round(t1-t0, 3)
+print "Training time: "+str(timeDiff)+"s"
+timeDiff = round(t2-t1, 3)
+print "Prediction time: "+str(timeDiff)+"s"
+print "Accuracy: "+str(acc)
+print "Precision: "+str(prec)
+print "Recall: "+str(rec)
