ca31cb1c6d4c602b599301ce627bc1455e67af9d
Mark Bannister <mspbannister@gmail.com>
2017-04-25 10:05:27 +0100

    Update 'outliers'

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -9,11 +9,24 @@ from feature_format import featureFormat, targetFeatureSplit
 
 ### read in data dictionary, convert to numpy array
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
+
+data_dict.pop('TOTAL')
+
 features = ["salary", "bonus"]
 data = featureFormat(data_dict, features)
 
 
 ### your code below
 
+for key, value in data_dict.items():
+    if value['bonus'] != "NaN" and value['bonus'] > 5000000 and value['salary'] > 1000000:
+        print key + " " + str(value['bonus'])
 
+for point in data:
+    salary = point[0]
+    bonus = point[1]
+    matplotlib.pyplot.scatter( salary, bonus )
 
+matplotlib.pyplot.xlabel("salary")
+matplotlib.pyplot.ylabel("bonus")
+matplotlib.pyplot.show()
--- outliers/outlier_removal_regression.py
+++ outliers/outlier_removal_regression.py
@@ -26,15 +26,16 @@ ages_train, ages_test, net_worths_train, net_worths_test = train_test_split(ages
 ### fill in a regression here!  Name the regression object reg so that
 ### the plotting code below works, and you can see what your regression looks like
 
+from sklearn.linear_model import LinearRegression
+reg = LinearRegression()
+reg.fit(ages_train, net_worths_train)
 
-
-
-
-
-
-
-
-
+# The coefficients
+print('Coefficients: \n', reg.coef_)
+# The intercept
+print('Intercept: \n', reg.intercept_)
+# Explained variance score: 1 is perfect prediction
+print('Variance score: %.3f' % reg.score(ages_test, net_worths_test))
 
 try:
     plt.plot(ages, reg.predict(ages), color="blue")
@@ -46,18 +47,14 @@ plt.show()
 
 ### identify and remove the most outlier-y points
 cleaned_data = []
+print len(cleaned_data)
+
 try:
     predictions = reg.predict(ages_train)
     cleaned_data = outlierCleaner( predictions, ages_train, net_worths_train )
 except NameError:
     print "your regression object doesn't exist, or isn't name reg"
     print "can't make predictions to use in identifying outliers"
-
-
-
-
-
-
 
 ### only run this code if cleaned_data is returning data
 if len(cleaned_data) > 0:
@@ -77,7 +74,13 @@ if len(cleaned_data) > 0:
     plt.xlabel("ages")
     plt.ylabel("net worths")
     plt.show()
-
+    
+    # The coefficients
+    print('Coefficients: \n', reg.coef_)
+    # The intercept
+    print('Intercept: \n', reg.intercept_)
+    # Explained variance score: 1 is perfect prediction
+    print('Variance score: %.3f' % reg.score(ages_test, net_worths_test))
 
 else:
     print "outlierCleaner() is returning an empty list, no refitting to be done"
