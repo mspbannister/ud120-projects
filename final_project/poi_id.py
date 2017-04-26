8000bd56d1c4bbc78d75e6737e3ff09e67a4fb71
Mark Bannister <mspbannister@gmail.com>
2017-04-26 15:17:14 +0100

    Update 'poi_id.py'

--- final_project/poi_id.py
+++ final_project/poi_id.py
@@ -2,6 +2,7 @@
 
 import sys
 import pickle
+import numpy as np
 sys.path.append("../tools/")
 
 from feature_format import featureFormat, targetFeatureSplit
@@ -10,19 +11,36 @@ from tester import dump_classifier_and_data
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
-features_list = ['poi','salary'] # You will need to use more features
+features_list = ['poi','total_stock_value', 'from_poi_to_this_person', 
+                 'from_poi_rate', 'to_poi_rate']
 
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
 
 ### Task 2: Remove outliers
+data_dict.pop('TOTAL', 0)
 ### Task 3: Create new feature(s)
-### Store to my_dataset for easy export below.
-my_dataset = data_dict
+my_dataset = {}
+for key in data_dict:
+    my_dataset[key] = data_dict[key]
+    try:
+        from_poi_rate = 1. * data_dict[key]['from_poi_to_this_person'] / \
+        data_dict[key]['to_messages']
+    except:
+        from_poi_rate = "NaN"
+    try:
+        to_poi_rate = 1. * data_dict[key]['from_this_person_to_poi'] / \
+        data_dict[key]['from_messages']
+    except:
+        from_poi_rate = "NaN"
+    my_dataset[key]['from_poi_rate'] = from_poi_rate
+    my_dataset[key]['to_poi_rate'] = to_poi_rate
 
 ### Extract features and labels from dataset for local testing
-data = featureFormat(my_dataset, features_list, sort_keys = True)
+data = featureFormat(my_dataset, features_list, remove_NaN=True, remove_all_zeroes=True, 
+                     remove_any_zeroes=True, sort_keys=True)
+
 labels, features = targetFeatureSplit(data)
 
 ### Task 4: Try a varity of classifiers
@@ -32,9 +50,11 @@ labels, features = targetFeatureSplit(data)
 ### http://scikit-learn.org/stable/modules/pipeline.html
 
 # Provided to give you a starting point. Try a variety of classifiers.
-from sklearn.naive_bayes import GaussianNB
-clf = GaussianNB()
+from sklearn.tree import DecisionTreeClassifier
+min_samples_split=2
+clf = DecisionTreeClassifier(min_samples_split=min_samples_split)
 
+    
 ### Task 5: Tune your classifier to achieve better than .3 precision and recall 
 ### using our testing script. Check the tester.py script in the final project
 ### folder for details on the evaluation method, especially the test_classifier
