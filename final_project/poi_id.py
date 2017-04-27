7f811e7ced5a5c04ae118b0f3776e2fbc792fd28
Mark Bannister <mspbannister@gmail.com>
2017-04-27 13:06:48 +0100

    Add final classifier to 'poi_id.py'

--- final_project/poi_id.py
+++ final_project/poi_id.py
@@ -1,25 +1,37 @@
 #!/usr/bin/python
+""" A script that loads data prepared by Katie Malone (Udacity)
+    relating to the Enron scandal, including financial and email
+    records for 145 individuals, and creates a decision tree classifier
+    that attempts to classify individuals based on whether they
+    were considered a 'person-of-interest' ("POI") or not.
+    Author: Mark Bannister (mspbannister@gmail.com)
+"""
+
 
 import sys
 import pickle
-import numpy as np
 sys.path.append("../tools/")
 
 from feature_format import featureFormat, targetFeatureSplit
-from tester import dump_classifier_and_data
+from tester import dump_classifier_and_data, test_classifier
 
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
-features_list = ['poi','total_stock_value', 'from_poi_to_this_person', 
-                 'from_poi_rate', 'to_poi_rate']
+
+features_list = ['poi', 'total_stock_value', 'from_poi_to_this_person', 
+                 'to_poi_rate']
 
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
 
 ### Task 2: Remove outliers
-data_dict.pop('TOTAL', 0)
+data_dict.pop('TOTAL', 0) # Contains column total data
+data_dict.pop('HUMPHREY GENE E', 0) # 'to_poi_rate' outlier
+data_dict.pop('DONAHUE JR JEFFREY M', 0) # 'from_poi_rate' outlier
+data_dict.pop('LAVORATO JOHN J', 0) # 'from_poi_to_this_person' outlier
+
 ### Task 3: Create new feature(s)
 my_dataset = {}
 for key in data_dict:
@@ -53,21 +65,11 @@ labels, features = targetFeatureSplit(data)
 # from sklearn.naive_bayes import GaussianNB
 # clf = GaussianNB()
 
-from sklearn.grid_search import GridSearchCV
-param_grid = {'criterion': ['gini', 'entropy'], 
-              'min_samples_split': [2, 4, 6, 8], 
-              'min_samples_leaf': [1, 2, 3, 4, 5],
-              'max_features': [1, 2, 3, 4]}#,
-              # 'n_estimators': [5, 10, 15, 20, 25, 50]}
-
-from sklearn.tree import DecisionTreeClassifier
-# min_samples_split=2
-algo = DecisionTreeClassifier()
+# from sklearn.tree import DecisionTreeClassifier
+# clf = DecisionTreeClassifier()
 
 # from sklearn.ensemble import RandomForestClassifier
-# algo = RandomForestClassifier()
-
-clf = GridSearchCV(algo, param_grid)
+# clf = RandomForestClassifier()
     
 ### Task 5: Tune your classifier to achieve better than .3 precision and recall 
 ### using our testing script. Check the tester.py script in the final project
@@ -76,10 +78,33 @@ clf = GridSearchCV(algo, param_grid)
 ### stratified shuffle split cross validation. For more info: 
 ### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
 
-# Example starting point. Try investigating other evaluation techniques!
-from sklearn.cross_validation import train_test_split
-features_train, features_test, labels_train, labels_test = \
-    train_test_split(features, labels, test_size=0.3, random_state=42)
+# from sklearn.grid_search import GridSearchCV
+# param_grid = {'criterion': ['gini', 'entropy'],
+#               'min_samples_split': [2, 4, 6, 8],
+#               'max_features': [2, 3, 4],
+#               'max_depth': [3, 4, 5, None]}
+#               # 'n_estimators': [5, 10, 15, 20]}
+
+# from sklearn.tree import DecisionTreeClassifier
+# algo = DecisionTreeClassifier()
+
+# from sklearn.ensemble import RandomForestClassifier
+# algo = RandomForestClassifier()
+
+# clf = GridSearchCV(algo, param_grid, scoring='recall', cv=4)
+
+# from sklearn.ensemble import RandomForestClassifier
+# clf = RandomForestClassifier(max_features=3, min_samples_split=4,
+#                               criterion='entropy', max_depth=None,
+#                               n_estimators=10)
+
+# Create final classifer
+from sklearn.tree import DecisionTreeClassifier
+clf = DecisionTreeClassifier(max_features=2, min_samples_split=4,
+                             criterion='entropy', max_depth=None)
+
+# Test using Udacity testing function
+# print test_classifier(clf, my_dataset, features_list, 1000)
 
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
